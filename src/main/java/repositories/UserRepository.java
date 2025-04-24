075d65ff89b253d10c08439b7bf0c0634f9461a2
viniciusprogr <vnsrodrigues10@gmail.com>
2025-04-24 04:29:18 -0300

    Update code UserRepository

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByDocument(String document);
-    Optional<User> findUserByDocument(Long id);
+    Optional<User> findUserById(Long id);
 }
